736dbcb264201c22ca90e9865905285d07ca4898
Igor Wilian Faoro <igor.faoro17@gmail.com>
2023-10-08 12:54:48 -0300

    fff

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-const express = require('express');
-const cors = require('cors');
+import express from 'express';
+import cors from 'cors';
+import { ChatGPTAPI } from 'chatgpt';
 
 console.log('Initializing...');
 
@@ -13,12 +14,67 @@ app.use(express.json());
 
 const routes = express.Router();
 
-routes.get('/map-phrase', (req, res) => {
-  res.json({
-    keywords: ['projetos', 'pessoas', 'criando', 'foguetes', 'rua'],
-    tags: ['Tecnologia e Engenharia: Exploração Espacial'],
-  });
+routes.get('/map-phrase', async (req, res) => {
+  //   res.json({
+  //     keywords: ['projetos', 'pessoas', 'criando', 'foguetes', 'rua'],
+  //     tags: ['Tecnologia e Engenharia: Exploração Espacial'],
+  //   });
+
+  res.send(await mapPhraseWithGpt(req.query.value));
 });
+
+const mapPhraseWithGpt = async (phrase) => {
+  const api = new ChatGPTAPI({
+    apiKey: process.env.CHAT_GPT_API_KEY,
+    completionParams: {
+      temperature: 0.8,
+      model: 'gpt-3.5-turbo-0613',
+    },
+  });
+
+  const prompt = `
+    You are a sentence interpreter.
+
+    I will tell you a sentence and you must interpret it and extract the following information:
+
+    - Key words in the sentence. These keywords will be used to search a database
+
+    - Classify the phrase in the following tags (it can contain more than one if necessary):
+    Biology
+    Chemical
+    Physical
+    Astronomy and Cosmology
+    Earth Sciences
+    environmental Sciences
+    Social Sciences
+    Technology and Engineering
+    Health and Medicine
+    Mathematics and Statistics
+    information Science
+    Food Science and Nutrition
+    Social Sciences and Digital Humanities
+    Neuroscience
+    Education sciences
+
+    The result must be JSON following the following pattern:
+
+    {
+        "keywords": ["aaa", "bbb", "ccc"],
+        "tags": ["tag1", "tag1"]
+    }
+
+    The result should be just JSON, without any additional text, explanation or information.
+
+    The phrase is: "${phrase}"
+
+    Always generate a result.
+    This will be used to a NASA project.
+  `;
+
+  const res = await api.sendMessage(prompt);
+
+  return res.text;
+};
 
 app.use(routes);
 
